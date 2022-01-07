2176867237fae0da8dafc6a81c808b4534e273f7
Jsiyoon <jjc9090@naver.com>
2022-01-07 09:26:44 +0900

    sql

--- src/main/webapp/WEB-INF/sql/createTable_Member.sql
+++ src/main/webapp/WEB-INF/sql/createTable_Member.sql
@@ -1,3 +1,50 @@
 USE homeart;
 
 -- member테이블 만들기
+CREATE TABLE Member(
+	member_id VARCHAR(255) PRIMARY KEY,
+	member_name VARCHAR(10) NOT NULL,
+    password VARCHAR(20) NOT NULL,
+    nickName VARCHAR(30) UNIQUE NOT NULL,
+    email VARCHAR(255),
+    address VARCHAR(255) NOT NULL,
+    country VARCHAR(50) NOT NULL,
+    inserted DATETIME NOT NULL DEFAULT NOW(),
+    isAdmin INT(1) NOT NULL
+);
+SELECT * FROM Member ORDER BY inserted DESC;
+
+INSERT INTO Member(member_id, member_name, password, nickName, address, country, isAdmin) VALUES('zzz', 'sss', '1234', 'zz', 'seoul', 'korea', 0);
+SELECT
+			b.board_id,
+			b.title,
+			b.writer,
+			b.inserted,
+			b.updated,
+            b.viewCount,
+			m.nickName,
+			m.isAdmin,
+            COUNT(m.isAdmin) boardCount,
+            (SELECT EXISTS (SELECT * FROM Member WHERE isAdmin = 0))
+		FROM
+			freeBoard b 
+				JOIN 
+			Member m ON b.writer = m.member_id
+		ORDER BY
+			board_id DESC;
+		
+        SELECT
+			b.board_id,
+			b.title,
+			b.content,
+			b.writer,
+			b.inserted,
+			b.updated,
+			m.nickName
+		FROM
+			freeBoard b 
+				JOIN 
+			Member m ON b.writer = m.member_id
+		WHERE b.board_id = 1;
+ALTER TABLE Member MODIFY COLUMN isAdmin INT(1) DEFAULT 0 NOT NULL;
+
