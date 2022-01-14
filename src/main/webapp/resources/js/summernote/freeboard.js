c7815608a12ed65e61aaccbbf1f1d38021f1a9e1
Jsiyoon <jjc9090@naver.com>
2022-01-14 19:47:02 +0900

    1/14 second commit

--- src/main/webapp/resources/js/summernote/freeboard.js
+++ src/main/webapp/resources/js/summernote/freeboard.js
@@ -1,11 +1,18 @@
-/**
- * 
- */
 $(document).ready(function() {
-		//textarea크기 스크롤 없이 글자에따른 크기 자동 조정
-	  $('textarea').keyup(function(e){
+	  //textarea크기 스크롤 없이 글자에따른 크기 자동 조정
+	  $('.post-title').keyup(function(e){
 		$(this).css('height', 'auto');
 		$(this).height(this.scrollHeight);
+	  });
+
+	  //글자수 제한
+	  $('.post-title').on('keyup', function() {
+	        $('#post_cnt').html("("+$(this).val().length+" / 100)");
+	 
+	        if($(this).val().length > 100) {
+	            $(this).val($(this).val().substring(0, 100));
+	            $('#post_cnt').html("(100 / 100)");
+	        }
 	  });
 	  
 	  $('#summernote').summernote({
@@ -38,15 +45,6 @@ $(document).ready(function() {
 	  });
 	  var value = $("input[name='isPublic']:checked").val();
 	  
-	  //글자수 제한
-	  $('.post-title').on('keyup', function() {
-	        $('#post_cnt').html("("+$(this).val().length+" / 100)");
-	 
-	        if($(this).val().length > 100) {
-	            $(this).val($(this).val().substring(0, 100));
-	            $('#post_cnt').html("(100 / 100)");
-	        }
-	});
 	// enter방지
 	$(document).keypress(function(e) { 
 		if (e.keyCode == 13) e.preventDefault(); 
